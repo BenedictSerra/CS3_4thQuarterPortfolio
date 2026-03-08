ebf21d185dfc19e4f7f4ca7c38a5cdfb188f6f39
BenedictSerra <b2029bjvserra@pshs.edu.ph>
2026-03-09 01:04:22 +0800

    2ND 1ST GRADED COMMIT

--- public/js/q41gBerylliumSERRA.js
+++ public/js/q41gBerylliumSERRA.js
@@ -1,41 +1,42 @@
-const stars = document.querySelectorAll(".star");
-const ratingText = document.getElementById("rating-text");
-const form = document.getElementById("form");
-const title = document.getElementById("title");
-const year = document.getElementById("year");
-const genre = document.getElementById("genre"); 
+const stars = document.querySelectorAll(".star");   //To get the span elements with star
+const form = document.getElementById("form"); //form
+const title = document.getElementById("title"); // title input 
+const year = document.getElementById("year"); // yyear input
+const genre = document.getElementById("genre"); //genre input
 
-let rating = 0;
+let rating = 0; // initializing rating number
 
-stars.forEach(star => {
-  star.addEventListener("click", () => {
-    rating = star.getAttribute("data-value");
+stars.forEach(star => {  // for each star
+  star.addEventListener("click", () => { // if star is clicked, make the rating equal to
+  // the star attribute "data-value"
+    rating = star.getAttribute("data-value"); // star 1 has data-value 1, star 2 has 2, etc.
 
     stars.forEach(s => {
       
       
-      if (s.getAttribute("data-value") <= rating) {
-        s.classList.add("active");
+      if (s.getAttribute("data-value") <= rating) { //checking each star again
+        s.classList.add("active"); // if star is data-value is <= rating, give .active
+        // which highlights the star gold
       } else {
-        s.classList.remove("active");
+        s.classList.remove("active"); // else remove the highlight
       }
     });
   });
 }); 
 
 function getRating() {
-    return JSON.parse(localStorage.getItem("rating")) || [];
+    return JSON.parse(localStorage.getItem("rating")) || []; // retrieving from storage
 }
 
 function saveRating(data) {
-    localStorage.setItem("rating", JSON.stringify(data))
+    localStorage.setItem("rating", JSON.stringify(data)) //saving to storage
 }
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function(e){ //on submit click after form
     
     const ratings = getRating();
 
-    if (rating==0) {
+    if (rating==0) { // if no rating is given
         e.preventDefault();
         alert("Please rate the movie!")
     }
@@ -43,47 +44,47 @@ form.addEventListener("submit", function(e){
     else {
         const confirmed = confirm("Are you sure about your rating?");
         if (!confirmed) {
-            e.preventDefault();
+            e.preventDefault(); // prevents from resetting form
             return;
         }
     }
 
 
-    const user = {
+    const user = { // saving in object
         title: title.value,
         year: year.value,
         genre: genre.value,
         rating: rating,
     };
 
-    ratings.push(user);
-    saveRating(ratings);
+    ratings.push(user); // pushing object to array
+    saveRating(ratings); // saving new ratings
 
     alert("Signup Saved!");
     form.reset();
-    displayMovies();
+    displayMovies(); // displays new movie ratings
 
 });
 
 
 function displayMovies() {
     const movieList = document.getElementById("movieList")
-    const ratings = getRating();
+    const ratings = getRating(); 
 
     movieList.innerHTML = "";
 
     ratings.forEach(movie => {
         let stars = "★".repeat(movie.rating) + "☆".repeat(5-movie.rating)
         
-        const movieItem = document.createElement("div");
-        movieItem.classList.add("movieItem");
+        const movieItem = document.createElement("div"); // gives a div to each movie item
+        movieItem.classList.add("movieItem"); 
         movieItem.innerHTML = `${movie.title} (${movie.year}) - ${movie.genre}, 
         Rating:      <span style="color:gold; font-size: 23px;">${stars}</span>`
 
-        movieList.appendChild(movieItem);
+        movieList.appendChild(movieItem); // adds new div to movielist
     })
 }
 
-displayMovies();
+displayMovies();  // when html page is loaded
 
 
